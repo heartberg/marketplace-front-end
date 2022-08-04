c53de32cd7af16c45da1a62adbacce25724a3ba3
Karen Stepanyan <kstepananyan@gmail.com>
2022-08-04 20:31:12 +0400

    Clickup task ID [2v78w0z]: Saved selected UI mode in storage.

--- Algoverse-app/src/app/app.component.ts
+++ Algoverse-app/src/app/app.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {ThemeService} from "./services/theme.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  isThemeChanged: boolean = false;
+export class AppComponent implements OnInit {
+  public appTheme: string = 'light';
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private readonly _themeService: ThemeService) {
     console.log(this.route.url);
   }
 
-  changeTheme(value: any) {
-   this.isThemeChanged = value;
-   return this.isThemeChanged;
+  ngOnInit(): void {
+    this.receiveThemeColor();
   }
 
+  private receiveThemeColor() {
+    this._themeService.getColorTheme();
+    this._themeService.$colorTheme.subscribe((theme: string) => {
+      this.appTheme = theme
+    })
+  }
 }
