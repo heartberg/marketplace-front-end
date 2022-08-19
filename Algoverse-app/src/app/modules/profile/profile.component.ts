4612f3c186cd1314af53d1b4dd3b98dc0b13abf7
tesco <heartberg@protonmail.com>
2022-08-19 18:01:04 +0400

    added numbers to tab on profile

--- Algoverse-app/src/app/modules/profile/profile.component.ts
+++ Algoverse-app/src/app/modules/profile/profile.component.ts
@@ -17,20 +17,17 @@ import { NgxSpinnerService } from 'ngx-spinner';
   providers: [Location]
 })
 export class ProfileComponent implements OnInit, OnDestroy{
-  profileTitle: string = 'Message text LOREM IPSUM Message text LOREM IPSUM Message text LOREM IPSUM Message text LOREM IPSUM Message text LOREM IPSUM Message text LOREM IPSUM Message text LOREM IPSUM Message text LOREM IPSUM Message text LOREM IPSUM '
-  profileImg: string = 'https://www.annualreviews.org/pb-assets/journal-home/special-collections/collection-archive-extreme-weather-2021-1630444709857.png'
-  public exampleArr:any = [];
   userProfile: any;
   userCollections: any;
-  myAssets: any;
-  createdAssets: any;
-  forSale: any;
-  forSwap: any;
-  forAuction: any;
-  myBids: any;
-  myAuctionBids: any;
-  swapRequests: any;
-  starred: any;
+  myAssets: any = [];
+  createdAssets: any = [];
+  forSale: any = [];
+  forSwap: any = [];
+  forAuction: any = [];
+  myBids: any = [];
+  myAuctionBids: any = [];
+  swapRequests: any = [];
+  starred: any = [];
   isOwnProfile: boolean = false;
   navigationSubscription: any;
   public isTruncated: boolean = true;
